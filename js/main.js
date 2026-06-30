10d4c353ef4adc5cb9ce79a96b70356df50492bd
maechul <maechul96@gmail.com>
2026-06-30 23:48:10 +0900

    hero

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 const intro = document.querySelector(".intro");
+const hero = document.querySelector(".hero");
 const firstSentence = document.querySelector(".intro__sentence--first");
 const secondSentence = document.querySelector(".intro__sentence--second");
 
-const showFirstSentence = 100;
-const showSecondSentence = 600;
-const fadeOutIntro = 3400;
-const hideIntro = 4200;
+const showFirstSentence = 200;
+const showSecondSentence = 800;
+const fadeOutIntro = 4000;
+const hideIntro = 5000;
 
 window.addEventListener("DOMContentLoaded", () => {
   window.setTimeout(() => {
@@ -18,6 +19,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
   window.setTimeout(() => {
     intro.classList.add("is-fading-out");
+    hero.classList.add("is-visible");
   }, fadeOutIntro);
 
   window.setTimeout(() => {
